a8e9a7cc0ea7416da96398b5eeaf587bf5cc9800
dyu <david.yu.ftw@gmail.com>
2018-01-24 23:30:41 +0800

    generated

---
+++ lib/util.d.ts
@@ -0,0 +1,10 @@
+export declare const enum ContentSlot {
+    TOP = 0,
+    BOTTOM = 1,
+}
+export declare const enum FormFlags {
+    PLACEHOLDER = 1,
+    REF_AND_ID = 2,
+    HORIZONTAL = 4,
+    TOGGLE_FLAG32 = 32,
+}
