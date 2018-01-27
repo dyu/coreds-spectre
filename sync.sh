94c489ae174e6db47c467c3968e44ef036c26fb5
dyu <david.yu.ftw@gmail.com>
2018-01-27 15:15:20 +0800

    include templates

--- sync.sh
+++ sync.sh
@@ -3,5 +3,9 @@
 [ ! -n "$1" ] && echo "1st arg (target_dir) is required." && exit 0
 
 rsync -avh --progress \
-      --include 'src/***' --include 'lib/***' --include 'scss/***' --exclude '*' \
+      --include 'src/***' \
+      --include 'lib/***' \
+      --include 'scss/***' \
+      --include 'templates/***' \
+      --exclude '*' \
       . $1/node_modules/coreds-spectre
